e5ff1478482b63662b10b5228e0eb5ee69a47a3b
github username <suoarik@gmail.com>
2019-01-15 19:52:27 +0000

    added predictive search

---
+++ client/src/components/ResultRenderer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ResultRenderer = (props) => {
+    return ( 
+    <div>
+           
+        {props.image_url ?
+        <div key='image' className='image'>
+            <img src={props.image_url} alt="profile=pic" />
+        </div>
+        :
+        null
+        }
+            <div key='content' className='content'>
+                {props.username && <div className='title'>{ props.username }</div>}
+                {props.first_name && <div className='description'>{ props.first_name } { props.last_name }</div>}
+            </div>
+          
+                  
+    </div> 
+    );
+}
+ 
+export default ResultRenderer;
